416f084a91b66bef46494d8895b16eb38413fec1
thorncp <thorncp@gmail.com>
2011-01-05 21:17:28 -0900

    further spec drying

--- spec/fsck/fsck_spec.rb
+++ spec/fsck/fsck_spec.rb
@@ -2,64 +2,54 @@ require "spec_helper"
 
 describe Fsck do
   before :each do
-    @class = Class.new do
-      include Fsck
-    end
+    @obj = Class.new { include Fsck }.new
   end
     
   describe "method is called" do
     it "should match method with fluff at the beginning" do
-      obj = @class.new
-      obj.should_receive(:the_method)
-      obj.call_the_method
+      @obj.should_receive(:the_method)
+      @obj.call_the_method
     end
     
     it "should match method with fluff at the end" do
-      obj = @class.new
-      obj.should_receive(:the_method)
-      obj.the_method_is_called
+      @obj.should_receive(:the_method)
+      @obj.the_method_is_called
     end
     
     it "should match method with fluff in the middle" do
-      obj = @class.new
-      obj.should_receive(:the_method)
-      obj.the_awesome_method
+      @obj.should_receive(:the_method)
+      @obj.the_awesome_method
     end
   end
   
   describe "return value" do
     it "should return the value from the method" do
-      obj = @class.new
-      obj.should_receive(:the_method).and_return(42)
-      obj.the_method_will_obey.should eql(42)
+      @obj.should_receive(:the_method).and_return(42)
+      @obj.the_method_will_obey.should eql(42)
     end
   end
   
   describe "method parameters" do
     it "should pass along all parameters" do
-      obj = @class.new
-      obj.should_receive(:the_method).with(42, [1,2,3], "yo dawg")
-      obj.the_awesome_method(42, [1,2,3], "yo dawg")
+      @obj.should_receive(:the_method).with(42, [1,2,3], "yo dawg")
+      @obj.the_awesome_method(42, [1,2,3], "yo dawg")
     end
   end
   
   describe "whacky method names" do
     it "should pass the regex when containing regex special characters" do
-      obj = @class.new
-      obj.should_receive(:[])
-      obj.send "square_[]_brackets"
+      @obj.should_receive(:[])
+      @obj.send "square_[]_brackets"
     end
     
     it "should not match within words" do
-      obj = @class.new
-      obj.should_not_receive(:the_method)
-      obj.the_methods
+      @obj.should_not_receive(:the_method)
+      @obj.the_methods
     end
     
     it "should allow fluff at the end of punctuated methods" do
-      obj = @class.new
-      obj.should_receive(:the_method?)
-      obj.the_method_will_be_called?
+      @obj.should_receive(:the_method?)
+      @obj.the_method_will_be_called?
     end
   end
 end
