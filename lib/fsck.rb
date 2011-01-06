a1443d8ecb5b8dc2c4e6e6b0c595bee53fb3d802
thorncp <thorncp@gmail.com>
2011-01-05 21:20:17 -0900

    now with lightning fast caching!
    totally useless (and possibly slower than before) caching, just for the hell of it

--- lib/fsck.rb
+++ lib/fsck.rb
@@ -1,13 +1,13 @@
 module Fsck
   def method_missing(sym, *args, &block)
-    method = sym.to_s
-    if punctuation = sym[/[!?=]$/]
-      method.chop!
+    fscked_method = sym.to_s
+    
+    if punctuation = fscked_method[/[!?=]$/]
+      fscked_method.chop!
     end
     
     matches = methods.select do |m|
-      words = m.to_s.delete("!?=").split("_").map { |w| Regexp.escape(w) }
-      method =~ /^(\w*_)?#{words.join('\w*_\w*')}(_\w*)?#{punctuation}$/
+      fscked_method =~ /^#{__fsck_cache__(m)}#{punctuation}$/
     end
 
     if matches.empty?
@@ -15,5 +15,15 @@ module Fsck
     else
       send(matches.sort_by(&:length).last, *args, &block)
     end
+  end
+  
+  private
+  def __fsck_cache__(method)
+    @_fsck_method_name_cache ||= {}
+    
+    return @_fsck_method_name_cache[method] if @_fsck_method_name_cache[method]
+    
+    words = method.to_s.delete("!?=").split("_").map { |w| Regexp.escape(w) }
+    @_fsck_method_name_cache[method] = "(\\w*_)?#{words.join('\w*_\w*')}(_\\w*)?"
   end
 end
